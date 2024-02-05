559ef8602c3327add347d54aeb9d81a5df2ebed8
tay1203 <tminghui1203@gmail.com>
2024-02-05 23:55:37 +0800

    javascript done

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-
-const model = {
+let model = {
     boardsize: 7,
     numShips: 3, // number of ships in board
     shipLength: 3, // length of a ship
     shipSunk: 0, // sunken ship
 
-    ships : [{ locations: ["10", "20", "30"], hits: ["", "", ""] },
-    { locations: ["32", "33", "34"], hits: ["", "", ""] },
-    { locations: ["63", "64", "65"], hits: ["", "", "hit"] } ],
+    ships : [ 
+        { locations: [0, 0, 0], hits: ["", "", ""] },
+        { locations: [0, 0, 0], hits: ["", "", ""] },
+        { locations: [0, 0, 0], hits: ["", "", ""] } 
+    ],
 
     // execute the fire action
     fire: function(guess) {
@@ -15,10 +16,14 @@ const model = {
             let ship = this.ships[i];
             let locations = ship.locations;
             let index = locations.indexOf(guess)
-            if (index >= 0) {
+            if (ship.hits[index] === "hit") {
+                view.displayMessage("Oops, you already hit that location!");
+                return true;
+            } else if (index >= 0) {
                 ship.hits[index] = "hit";
                 view.displayHit(guess);
                 view.displayMessage("HIT!");
+
                 if (this.isSunk(ship)) {
                     view.displayMessage("You sank my battleship!");
                     this.shipSunk++
@@ -39,11 +44,57 @@ const model = {
             }
         }
         return true;
+    },
+
+    generateShipLocations: function() {
+        let locations;
+
+        // for each ship
+        for (let i = 0; i < this.numShips; i++) {
+            do {
+                // generate new set of location
+                locations = this.generateShip();
+            } while (this.collisions(locations));
+            // check if there is no overlap
+            // if do, keep generate and check until no
+            // assign the generated location to the ship's 'location' property
+            this.ships[i].locations = locations;
+        }
+    },
+
+    generateShip: function() {
+        // generate horizontally only
+        // can be in any row
+        let row = Math.floor(Math.random() * this.boardsize);
+        // leave 2 space for column, generate 0 - 4
+        let col = Math.floor(Math.random() * (this.boardsize - this.shipLength + 1));
+
+        let newShipLocations = [];
+        for (let i = 0; i < this.shipLength; i++) {
+            // add the location into the array like ["01", "02", "03"]
+            newShipLocations.push(row + "" + (col + i));
+        }
+        return newShipLocations;
+    },
+
+    collisions: function(locations){
+        for (let i = 0; i < this.numShips; i++) {
+            // check the ships on the board one by one
+            let ship = model.ships[i];
+            for (let j = 0; j < locations.length; j++){
+                // check the input location is exists or not
+                // if exist, there should return an valid index within the range  
+                if (ship.locations.indexOf(locations[j]) >= 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
-}
+};
 
-const view = {
+let view = {
     displayMessage: function(msg){
         let messageArea = document.getElementById("messageArea");
         messageArea.innerHTML = msg;
@@ -58,44 +109,82 @@ const view = {
         let cell = document.getElementById(location);
         cell.setAttribute("class", "miss");
     }
-}
+};
 
-const controller = {
+let controller = {
     guesses: 0,
 
     processGuess: function(guess) {
+        let location = this.parseGuess(guess)
 
+        // if return value is not null, its valid
+        if (location) {
+            this.guesses++;
+            // handle the valid location to process hit/miss
+            let hit = model.fire(location);
+            // if all ship are sunk, process game over
+            if (hit && model.shipSunk === model.numShips){
+                view.displayMessage("You sank all my battleships, in " + this.guesses + " guesses");
+            }
+        }
     },
 
     parseGuess:function(guess){
         let char = ["A", "B", "C", "D", "E", "F", "G"];
+        // validate input if null or not 2 char
         if (guess == null || guess.length !== 2) {
             view.displayMessage("Oops, please enter a letter and a number on the board.");
         } else {
             let firstChar = guess.charAt(0);
+            // if not exist = -1
             let row = char.indexOf(firstChar);
             let col = guess.charAt(1);
-
+            
+            // validate numbers
             if (isNaN(row) || isNaN(col)){
+                // check if not num
                 view.displayMessage("Oops, that isn't on the board.");
             } else if (row < 0 || row >= model.boardsize || col < 0 || col >= model.boardsize ) {
+                // check if not in board size range
                 view.displayMessage("Oops, that's off the board!");
             } else {
+                // concatenation
                 return row + col;
             }
-
         }
-
+        return null;
     }
 }
 
-// model.fire("53");
-// model.fire("06");
-// model.fire("16");
-// model.fire("26");
-// model.fire("34");
-// model.fire("24");
-// model.fire("44");
-// model.fire("12");
-// model.fire("11");
-// model.fire("10");
+// called when the page has completed loading
+window.onload = init;
+
+function init(){
+    let fireButton = document.getElementById("fireButton");
+    // call fire button function 
+    fireButton.onclick = handleFireButton;
+
+    let guessInput = document.getElementById("guessInput");
+    guessInput.onkeydown = handleKeyPress;
+
+   model.generateShipLocations();
+}
+
+// event handler
+
+function handleFireButton(){
+    let guessInput = document.getElementById("guessInput");
+    controller.processGuess(guessInput.value.toUpperCase());
+    guessInput.value = "";
+}
+
+// browser pass an event object, abt which key is pressed
+function handleKeyPress(e) {
+    let fireButton = document.getElementById("fireButton");
+    if (e.keyCode === 13) {
+        fireButton.click();
+        // form does not do anything
+        return false;
+    }
+}
+
