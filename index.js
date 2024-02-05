7c2a9da91551590230e5704e1b25d7f26f8fe8f4
tay1203 <tminghui1203@gmail.com>
2024-02-05 17:08:02 +0800

    update html and js logic

--- index.js
+++ index.js
@@ -1,45 +1,101 @@
-// a ship holds 3 cells
-let shipHead = Math.floor(Math.random()*5);
-let shipBody = shipHead - 1;
-let shipTail = shipHead - 2;
 
-let userGuessCounter = 0;
-let guessLimit = 7;
-let userGuess;
+const model = {
+    boardsize: 7,
+    numShips: 3, // number of ships in board
+    shipLength: 3, // length of a ship
+    shipSunk: 0, // sunken ship
 
-// the state of the ship
-let isSunk = false;
-let numOfHits = 0;
+    ships : [{ locations: ["10", "20", "30"], hits: ["", "", ""] },
+    { locations: ["32", "33", "34"], hits: ["", "", ""] },
+    { locations: ["63", "64", "65"], hits: ["", "", "hit"] } ],
 
-while(isSunk == false){
-    if(userGuessCounter <= guessLimit){
-        userGuess = prompt('Ready, aim, fire! (enter a number 0-6): ');
-
-        
-            if(0 <= userGuess && userGuess <= 6){
-                if(shipHead <= userGuess && userGuess <= (shipHead + 2)){
-                    numOfHits += 1
-                    alert('You hit the ship ' + numOfHits +' times! ');
-                    if(numOfHits == 3){
-                        isSunk = true;
-                        alert('You sank my battleship!')
-                    }
+    // execute the fire action
+    fire: function(guess) {
+        for (let i = 0; i < this.numShips; i++) {
+            let ship = this.ships[i];
+            let locations = ship.locations;
+            let index = locations.indexOf(guess)
+            if (index >= 0) {
+                ship.hits[index] = "hit";
+                view.displayHit(guess);
+                view.displayMessage("HIT!");
+                if (this.isSunk(ship)) {
+                    view.displayMessage("You sank my battleship!");
+                    this.shipSunk++
                 }
-                else{
-                    alert('MISS!')
-                }
-                userGuessCounter += 1;
-            } else{
-                alert('Please enter a valid number from 1 to 7.');
+                return true;
             }
-    } else{
-        alert('You are out of chances! ');
-        break;
+        }
+        view.displayMiss(guess);
+        view.displayMessage("You missed.");
+        return false;
+    },
+
+    // verify the ship is sunk or not
+    isSunk: function(ship) {
+        for (let i = 0; i < this.shipLength; i++) {
+            if (ship.hits[i] !== "hit"){
+                return false;
+            }
+        }
+        return true;
     }
-    
+
 }
 
-let stats = "You took " + userGuessCounter + " guesses to sink the battleship, " + "your shooting accuracy was " + (3/userGuessCounter);
-if(isSunk == true){
-    alert(stats);
+const view = {
+    displayMessage: function(msg){
+        let messageArea = document.getElementById("messageArea");
+        messageArea.innerHTML = msg;
+    },
+
+    displayHit: function(location){
+        let cell = document.getElementById(location);
+        cell.setAttribute("class", "hit");
+    },
+
+    displayMiss: function(location){
+        let cell = document.getElementById(location);
+        cell.setAttribute("class", "miss");
+    }
 }
+
+const controller = {
+    guesses: 0,
+
+    processGuess: function(guess) {
+
+    },
+
+    parseGuess:function(guess){
+        let char = ["A", "B", "C", "D", "E", "F", "G"];
+        if (guess == null || guess.length !== 2) {
+            view.displayMessage("Oops, please enter a letter and a number on the board.");
+        } else {
+            let firstChar = guess.charAt(0);
+            let row = char.indexOf(firstChar);
+            let col = guess.charAt(1);
+
+            if (isNaN(row) || isNaN(col)){
+                view.displayMessage("Oops, that isn't on the board.");
+            } else if (row < 0 || row >= model.boardsize || col < 0 || col >= model.boardsize ) {
+                view.displayMessage("Oops, that's off the board!");
+            } else {
+                return row + col;
+            }
+
+        }
+
+    }
+}
+
+// model.fire("53");
+// model.fire("06");
+// model.fire("16");
+// model.fire("26");
+// model.fire("34");
+// model.fire("24");
+// model.fire("44");
+// model.fire("12");
+// model.fire("11");
+// model.fire("10");
